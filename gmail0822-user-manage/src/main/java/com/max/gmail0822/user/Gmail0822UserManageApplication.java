54f15231780c3b1f3eff6e56e62bde093e39a009
MaxwellBest <bart30508@gmail.com>
2020-08-23 09:48:31 +0800

    加入用戶管理Module

---
+++ gmail0822-user-manage/src/main/java/com/max/gmail0822/user/Gmail0822UserManageApplication.java
@@ -0,0 +1,13 @@
+package com.max.gmail0822.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmail0822UserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmail0822UserManageApplication.class, args);
+    }
+
+}
